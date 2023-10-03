1a15e8c4e5eeea15bfaf9bf0d7703ccbfdec5c53
Taufik Ferdous <112876342+Taufik-Ferdous@users.noreply.github.com>
2023-10-03 19:08:10 +0600

    Update 58A.cpp

--- 58A.cpp
+++ 58A.cpp
@@ -1,54 +1,28 @@
-//!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-//!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-//!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-//!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-#include <bits/stdc++.h>
-#define endl "\n"
-
+#include<bits/stdc++.h>
 using namespace std;
 
-int main()
-{
-    int t;
-    cin >> t;
-    vector<int> ol, ll, lo;
-    while (t--)
-    {
-        int n;
-        cin >> n;
+int main(){
+    string in;
+    int ck = 0;
+    cin>>in;
 
-        for (int i = 0; i < n; i++)
-        {
-            int s;
-            string a;
-            cin >> s >> a;
-            if (a == "11")
-                ll.push_back(s);
-            else if (a == "10")
-                lo.push_back(s);
-            else if (a == "01")
-                ol.push_back(s);
-        }
-
-        sort(ol.begin(), ol.end());
-        sort(lo.begin(), lo.end());
-        sort(ll.begin(), ll.end());
-
-        if(lo[0]!=0 and ol[0]!=0 and ll[0]!=0){
-            if ((lo[0]+ol[0]) > ll[0]) cout<<ll[0] <<endl;
-            else cout<<lo[0]+ol[0] <<endl;
-        }
-
-        //else if (ll[0]==0 and (lo[0]==0 or ol[0]==0)) cout<<-1 <<endl;
-        
-        else if(ll[0]!=0 and (lo[0]==0 or ol[0]==0)) cout<<ll[0] <<endl;
-
-        else if (ll[0]==0 and lo[0]!=0 and ol[0]!=0) cout<<lo[0]+ol[0] <<endl;
-
-        else cout<<-1 <<endl;
-
-        ll.clear();
-        lo.clear();
-        ol.clear();
+    for(int i=0; i<in.length(); i++){
+        if(in[i] == 'h' && ck == 0)
+            ck++;
+        else if(in[i] == 'e' && ck == 1)
+            ck++;
+        else if(in[i] == 'l' && ck == 2)
+            ck++;
+        else if(in[i] == 'l' && ck == 3)
+            ck++;
+        else if(in[i] == 'o' && ck == 4)
+            ck++;
     }
+
+    if(ck == 5)
+        cout<<"YES" <<endl;
+    else
+        cout<<"NO" <<endl;
+
+    return 0;
 }
